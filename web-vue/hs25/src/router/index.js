a2fe088ec9645739f62fcfb6cfa2c670af18dbad
Jess Long <jessica.long@coldbanana.com>
2025-02-22 21:50:24 +0000

    network

--- web-vue/hs25/src/router/index.js
+++ web-vue/hs25/src/router/index.js
@@ -2,6 +2,7 @@ import { createRouter, createWebHistory } from 'vue-router'
 import LessonView from '../views/LessonView.vue'
 import CardView from '../views/CardView.vue'
 import BrunoTransition from '@/views/BrunoTransition.vue'
+import NetworkView from '@/views/NetworkView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -20,6 +21,11 @@ const router = createRouter({
       path: '/bruno',
       name: 'bruno',
       component: BrunoTransition,
+    },
+    {
+      path: '/network',
+      name: 'network',
+      component: NetworkView,
     },
   ],
 })
