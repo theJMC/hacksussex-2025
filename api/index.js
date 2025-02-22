0bfc1b2237e7b461d0740d79bea158946ad5a5a8
James <james@jamesmcc.co.uk>
2025-02-22 18:32:31 +0000

    added cors

--- api/index.js
+++ api/index.js
@@ -1,7 +1,9 @@
 const express = require('express')
+const cors = require('cors')
 const app = express()
 const port = 3001
 
+app.use(cors())
 const fs = require("node:fs")
 
 require('dotenv').config()
