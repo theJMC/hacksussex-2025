9a2d3962f76b2e830bf517716d4588121d22e33f
James <james@jamesmcc.co.uk>
2025-02-22 12:45:52 +0000

    added css stuff

--- web/index.js
+++ web/index.js
@@ -2,6 +2,8 @@ const express = require('express')
 const app = express()
 const port = 3000
 
+app.use(express.static('css'))
+
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/www/index.html');
 })
