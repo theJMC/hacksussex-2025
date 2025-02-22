69203ba1d25ea7f41b7f33d5e6a631e343787000
James <james@jamesmcc.co.uk>
2025-02-22 12:43:17 +0000

    initial web stuff

---
+++ web/index.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/www/index.html');
+})
+
+app.get('/:file', (req, res) => {
+    res.sendFile(__dirname + '/www/' + req.params.file + '.html');
+})
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+})
